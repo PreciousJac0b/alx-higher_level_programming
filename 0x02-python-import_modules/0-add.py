26a97b868d8779c68f4ba467076ed4448dcbbcc7
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-08-25 05:17:32 -0700

    Python script that adds two numbers

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,7 +4,8 @@ from add_0 import add
 def main():
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    c = a + b
+    print("{} + {} = {}".format(a, b, c))
 
 if __name__ = '__main__':
     main()
