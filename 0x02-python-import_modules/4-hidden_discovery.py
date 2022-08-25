dea2613a67fb32432db962a54b9590d81ca9ae8e
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-08-25 07:48:33 -0700

    Hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-import hidden_4
 
 
 def main():
     lists = dir(hidden_4)
     for i in range(len(lists)):
         if lists[i][:2] != '__':
-            print("{}".format(lists[i]))
+            print(lists[i])
 
 
 if __name__ == '__main__':
+    import hidden_4
     main()
