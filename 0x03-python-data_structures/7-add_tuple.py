227f4925b0184b901fba23650603a37288bf5f98
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-08-26 22:17:08 -0700

    Adds tuple, resolved checker errors

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,8 +6,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a += (0,)
     elif len(tuple_a) == 0:
         tuple_a += (0, 0)
-    elif len(tuple_b) == 1:
+
+    if len(tuple_b) == 1:
         tuple_b += (0,)
     elif len(tuple_b) == 0:
         tuple_b += (0, 0)
+
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
