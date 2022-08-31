6dc8466c4d9d6611ea3d30b65628fc64a45d45ac
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-08-31 06:47:00 -0700

    Multiplies dictionary values by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def multiply_by_2(a_dictionary):
+    new_dict = {}
+    for elem in a_dictionary.keys():
+        new_dict[elem] = a_dictionary[elem] * 2
+    return new_dict
