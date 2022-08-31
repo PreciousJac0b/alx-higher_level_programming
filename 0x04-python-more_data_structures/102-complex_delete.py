b5f3abde988e9b520f8ac5979875b6b05832eec7
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-08-31 08:48:57 -0700

    Complex deletion of keys

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,7 +4,7 @@
 def complex_delete(a_dictionary, value):
     if not value or value not in a_dictionary.values():
         return a_dictionary
-    new_dict = a_dictionary.keys()
     for elem in list(a_dictionary):
         if a_dictionary[elem] == value:
             del a_dictionary[elem]
+    return a_dictionary
