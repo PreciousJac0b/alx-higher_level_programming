e38590b409ad01bdadffd3bc09eed4f8ec1fda18
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-08-31 06:41:54 -0700

    Deletes existing key in a dictionary

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,7 +2,7 @@
 
 
 def simple_delete(a_dictionary, key=""):
-    if not key:
+    if not key or key not in a_dictionary:
         return a_dictionary
     del a_dictionary[key]
     return a_dictionary
