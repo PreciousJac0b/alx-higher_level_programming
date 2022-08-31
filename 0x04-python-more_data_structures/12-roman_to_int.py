6c37e40e56420a4b7ca3ed71cef52d2795c5f47e
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-08-31 11:29:15 -0700

    Converts roman numeral to integers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 
 
 def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) is not str:
+        return 0
     rod = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     decimal_number = 0
     if len(roman_string) == 1:
