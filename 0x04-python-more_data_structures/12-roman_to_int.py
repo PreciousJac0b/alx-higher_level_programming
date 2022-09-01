c951f460f2498cb3adf9531ba65699ec37593bdc
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-09-01 04:11:13 -0700

    Roman to integer checker error rectified

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,15 +6,17 @@ def roman_to_int(roman_string):
         return 0
     rod = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     decimal_number = 0
+    i = 0
     if len(roman_string) == 1:
         return decimal_number + rod[roman_string[0]]
-    for i in range(len(roman_string) - 2):
+    while i < (len(roman_string) - 2):
         if rod[roman_string[i]] >= rod[roman_string[i + 1]]:
             decimal_number += rod[roman_string[i]]
+            i += 1
         else:
             first = rod[roman_string[i]]
             decimal_number += abs(first - rod[roman_string[i + 1]])
-            i += 1
+            i += 2
     if rod[roman_string[-2]] < rod[roman_string[-1]]:
         decimal_number += abs(rod[roman_string[-2]] - rod[roman_string[-1]])
     else:
