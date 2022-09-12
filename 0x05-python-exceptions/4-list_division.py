d6f5cecc6365cb171b2792af6bd2fea05a786f1a
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-09-12 08:37:05 -0700

    checker error fixed

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,23 +3,18 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    for q, r in zip(my_list_1, my_list_2):
+    for i in range(list_length):
         try:
-            value = q / r
+            value = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             value = 0
             print("division by 0")
         except TypeError:
             value = 0
             print("wrong type")
+        except IndexError:
+            value = 0
+            print("out of range")
         finally:
             new_list.append(value)
-    if len(my_list_1) != len(my_list_2):
-        print("out of range")
-        ranged = abs(len(my_list_1) - len(my_list_2))
-        for i in range(ranged):
-            new_list.append(0)
-    if list_length > len(new_list):
-        for i in range(list_length - len(new_list):
-                new_list.append(0)
     return new_list
