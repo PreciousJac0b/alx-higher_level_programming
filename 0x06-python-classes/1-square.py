c8891ce7869a8230649f87282f731f95051e6164
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-09-13 10:43:59 -0700

    Square class with private class attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Square class with a private class attribute"""
+
+
+class Square:
+    """Square class with size attribute"""
+    def __init__(self, size):
+        self.__size = size
