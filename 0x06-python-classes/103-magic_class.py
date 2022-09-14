7562f1f7ec5675abf2aa28d65531853b089e33bd
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-09-13 17:35:47 -0700

    resolved checker error

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
-
-"""Defines a Magic Circle Class"""
-
 import math
 
 
 class MagicClass:
-    """Represents a circle"""
-
     def __init__(self, radius=0):
-        """Initializes MagicClass
-
-            Args:
-                radius(int or float): Radius of MagicClass
-        """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius is not a number")
         self.__radius = radius
 
     def area(self):
-        """Returns area of circle"""
         return (self.__radius ** 2) * math.pi
 
     def circumference(self):
-        """Returns circumference of circle"""
         return 2 * math.pi * self.__radius
