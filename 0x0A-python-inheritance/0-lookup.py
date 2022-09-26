5b47a6d5bcd6008cb853aab8ddd938a8549b4c9d
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-09-26 02:34:03 -0700

    lists all attributes in an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+Lists the available attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """Prints the available attributes of an object"""
+    return dir(obj)
