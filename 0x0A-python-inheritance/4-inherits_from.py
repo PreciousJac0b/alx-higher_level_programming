b0458e88702ab3997e1ddb3aff7977939cfc9fc4
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-09-26 04:31:07 -0700

    Inherits from class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+A function that evaluates subclasses
+"""
+
+
+def inherits_from(obj, a_class):
+    """Returns True if obj is a subclass of a_class"""
+    if issublass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
