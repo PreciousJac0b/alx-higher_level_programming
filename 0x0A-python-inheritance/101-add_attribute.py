90d1cb8465a9e2fcd870d176e275487cc8c79ae1
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-09-26 11:46:05 -0700

    101 add attribute

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Represents a class"""
+
+
+def add_attribute(first, second, third):
+    """
+    Adds attribute to a class if possible
+    """
+    if not hasattr(first, "__dict__"):
+        raise TypeError("can't add attribute")
+    setattr(first, second, third)
