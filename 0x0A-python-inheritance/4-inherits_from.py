d6f43abd36f0c82c14bfdf10696a93f9cec30f6c
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-09-26 14:44:24 -0700

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,8 +7,4 @@ A function that evaluates subclasses
 
 def inherits_from(obj, a_class):
     """Returns True if obj is a subclass of a_class"""
-    if issublass(type(obj), a_class):
-        return True
-    elif isinstance(obj, a_class):
-        return True
-    return False
+    return type(obj) is not a_class and isinstance(obj, a_class)
