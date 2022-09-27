6b3dea446fc5f44478eb916ac95805f7388053f3
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-09-27 08:56:19 -0700

    Fixed Pycodestyle Error

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,12 +5,12 @@
 if __name__ == '__main__':
     import json
     import sys
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    load_from_json = __import__('6-load_from_json_file').load_from_json_file
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
     try:
-        new_list = load_from_json_file('add_item.json')
-    except:
+        new_list = load_from_json('add_item.json')
+    except FileNotFoundError:
         new_list = []
     finally:
         new_list.extend(sys.argv[1:])
