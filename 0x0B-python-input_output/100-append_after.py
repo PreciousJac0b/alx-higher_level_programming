c02f1d62adf537a4b5eb9361604476dfca91bc5e
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-09-27 14:53:09 -0700

    100-append_after.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Appends a string after some lines in a file"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Checks if the lines in a particular file contains search string
+
+    Appends new_string to the end of a line if it does contain it
+    """
+    r_string = ""
+    with open(filename, 'r', encoding="utf-8") as f:
+        for line in f:
+            r_string += line
+            if search_string in line:
+                r_string += new_string
+    with open(filename, 'w', encoding="utf-8") as fd:
+        fd.write(text)
