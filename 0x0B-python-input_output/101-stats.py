5a0feff0283c8e520b038e4c20232f8f6d2c6458
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-09-28 16:45:57 -0700

    Log parser

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+if __name__ == '__main__':
+    import fileinput
+
+    def print_dict(status, size):
+        print("File size: {}".format(size))
+        status = dict(sorted(status.items()))
+        for elem in status:
+            print("{}: {}".format(elem, status[elem]))
+    i = 0
+    file_size = 0
+    status_code = {}
+    for line in fileinput.input():
+        if i % 10 == 0 and i != 0:
+            print_dict(status_code, file_size)
+        line = line.split()
+        code = line[-2]
+        size = line[-1]
+        if not code:
+            continue
+        if code not in status_code:
+            status_code[code] = 1
+        else:
+            status_code[code] += 1
+        file_size += int(size)
+        i += 1
+    print_dict(status_code, file_size)
