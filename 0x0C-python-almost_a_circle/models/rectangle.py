4e2d7f41b7167a862fe4f6935a0a250cdf623ec4
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-09-29 04:20:07 -0700

    Implemented a method that computes the area of a rectangle instance

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,3 +83,10 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """
+        Computes and returns the area of the 
+        rectangle class instance
+        """
+        return self.__width * self.__height
