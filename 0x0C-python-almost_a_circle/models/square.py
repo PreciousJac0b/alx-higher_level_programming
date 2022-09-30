6b501a8315cd93584adc5cec7b38cd18f1fb95c5
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-09-30 04:16:32 -0700

    Updated the class Square with public getter and setter size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -21,3 +21,17 @@ class Square(Rectangle):
         """Displays the square class"""
         return "[{}] ({}) {}/{} - {}".format(self.__class__.__name__,
                                             self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """size field getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """The size attribute decorator setter method"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = self.height = value
