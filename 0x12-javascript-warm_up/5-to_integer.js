e8dbb8e5513b6d0551d72a5a57e0fa0c57769b67
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-11-28 15:55:22 +0100

    betty checker

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,6 +4,6 @@ const argv = process.argv;
 if (Number.isInteger(argv[2])) {
   console.log('Not a number');
 } else {
-  const n = parseInt(argv[2]);
+  const n = Math.floor(Number(argv[2]));
   console.log(`My number: ${n}`);
 }
