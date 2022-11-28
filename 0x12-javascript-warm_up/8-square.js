d502b0a7b1bee56e653d1759bf51b8578252f531
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-11-28 16:31:34 +0100

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,9 +2,13 @@
 
 const arg = Math.floor(Number(process.argv[2]));
 
-for (let i = 0; i < arg; i++) {
-  for (let j = 0; j < arg; j++) {
-    process.stdout.write('X');
+if (!Number.isInteger(arg)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < arg; i++) {
+    for (let j = 0; j < arg; j++) {
+      process.stdout.write('X');
+    }
+    console.log();
   }
-  console.log();
 }
