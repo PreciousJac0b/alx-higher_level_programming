f9bb4181e3a98b5fe37ded7128e7cf40a69e3176
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-11-28 16:52:21 +0100

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,7 +5,5 @@ const args = process.argv;
 function add (a, b) {
   return a + b;
 }
-if (args.length < 3) {
-  console.log('NaN');
-}
+
 console.log(add(Math.floor(Number(args[2])), Math.floor(Number(args[3]))));
