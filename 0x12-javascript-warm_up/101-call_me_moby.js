bc05544732dd760019e44e40159dd89755dad183
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-11-29 01:51:18 +0100

    101-call_me_moby.js

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
-exports.addMeMaybe = function (number, theFunction) {
-	  theFunction(++number);
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) theFunction();
 };
