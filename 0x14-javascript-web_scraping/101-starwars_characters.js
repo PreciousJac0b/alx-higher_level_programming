94e174f7668454be96d6cff27c131c4f4301f629
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-01-17 19:01:09 -0500

    Prints all Starwars characters from the star wars API using async await Promise

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,14 +5,17 @@
 const request = require('request');
 const apiUrl = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-request.get(apiUrl, function (error, response, body) {
+request.get(apiUrl, async function (error, response, body) {
   if (error) throw error;
   const characters = JSON.parse(body).characters;
   for (const elem of characters) {
-    request.get(elem, function (errors, responses, bodies) {
-      if (errors) throw errors;
-      const result = JSON.parse(bodies).name;
-      console.log(result);
+    await new Promise((resolve, reject) => {
+      request.get(elem, function (errors, responses, bodies) {
+        if (errors) reject(errors);
+        const result = JSON.parse(bodies).name;
+        console.log(result);
+        resolve();
+      });
     });
   }
 });
