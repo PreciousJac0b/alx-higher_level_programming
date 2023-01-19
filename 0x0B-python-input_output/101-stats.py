2913db41cb7697d697752aba62b98c0a46af4ac2
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-01-19 13:18:49 -0500

    Handled Keyboard Exception

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
                 continue
             tmp = line[-2]
             if ord(tmp[0]) not in list(range(48, 57)):
-                sys.stderr.write("Invalid Input")
+                sys.stderr.write("")
                 continue
             code = line[-2]
             size = line[-1]
