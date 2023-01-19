96ecc0fad44eda079c7de1fc65a813d1444757fb
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-01-19 12:50:37 -0500

    Handled Keyboard Exception

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,6 +6,7 @@ and number of times status code occured after every 10 lines
 
 if __name__ == '__main__':
     import fileinput
+    import sys
 
     def print_dict(status, size):
         """Prints a dictionary in sorted order"""
@@ -13,29 +14,34 @@ if __name__ == '__main__':
         status = dict(sorted(status.items()))
         for elem in status:
             print("{}: {}".format(elem, status[elem]))
-    i = 0
-    file_size = 0
-    status_code = {}
-    new_list = [200, 301, 400, 401, 403, 404, 405, 500]
-    for line in fileinput.input():
-        if i % 10 == 0 and i != 0:
-            print_dict(status_code, file_size)
-        line = line.split()
-        if len(line) < 2:
-            continue
-        try:
-            code = line[-2]
-            size = line[-1]
-        except IndexError:
-            pass
-        if not code:
-            continue
-        if int(code) not in new_list:
-            continue
-        if code not in status_code:
-            status_code[code] = 1
-        else:
-            status_code[code] += 1
-        file_size += int(size)
-        i += 1
-    print_dict(status_code, file_size)
+
+    try:
+        i = 0
+        file_size = 0
+        status_code = {}
+        new_list = [200, 301, 400, 401, 403, 404, 405, 500]
+        for line in fileinput.input():
+            if i % 10 == 0 and i != 0:
+                print_dict(status_code, file_size)
+            line = line.split()
+            if len(line) < 2:
+                continue
+            try:
+                code = line[-2]
+                size = line[-1]
+            except IndexError:
+                pass
+            if not code:
+                continue
+            if int(code) not in new_list:
+                continue
+            if code not in status_code:
+                status_code[code] = 1
+            else:
+                status_code[code] += 1
+            file_size += int(size)
+            i += 1
+        print_dict(status_code, file_size)
+    except KeyboardInterrupt:
+        print_dict(status_code, file_size)
+        sys.exit(1)
